26a5827470ef1f71bf6204e60d45a67f8de8cd17
Olivia Prior <olivialaineprior@gmail.com>
2018-11-07 21:14:13 -0500

    Updated playback rates based off of the pressure from the sensor

--- sketch.js
+++ sketch.js
@@ -1,16 +1,23 @@
-var playing = false;
+var playing1 = false;
+var playing2 = false;
 var slowly1;
 var slowly2;
+var force = {};
 var button;
-
-var serial;       //variable to hold the serial port object
+var playback1;
+var playback2;
+var newPlayback1;
+var nePlayback2;
+var range;
+var leftVid;
+var rightVid;
+var serial;  //variable to hold the serial port object
+var timer = 0;     // used to time
 var ardVal = [];  //array that will hold all values coming from arduino
 
 var serialPortName = '/dev/cu.usbmodem1411';        //FOR PC it will be COMX on mac it will be something like "/dev/cu.usbmodemXXXX"
                                     //Look at P5 Serial to see the available ports
 
-// TODO: create a true false variable to determine if the video is playing
-// TODO: try and find a way to see where the video is in the loop (time)
 // TODO: connect arduino values to the videos
 
 
@@ -27,45 +34,94 @@ function setup() {
   slowly1 = createVideo(['slowly.mp4']);
   slowly1.id("slowly1");
   slowly1.size(360, 360);
+
   slowly2 = createVideo(['slowly.mp4']);
   slowly2.size(360, 360);
   slowly2.id("slowly2");
 
-  button = createButton('play');
-  button.mousePressed(play_speed); // attach button listener
-
-  button = createButton('2x speed');
-  button.position(100, 68);
-  button.mousePressed(twice_speed);
-
-  button = createButton('half speed');
-  button.position(200, 68);
-  button.mousePressed(half_speed);
-
-  button = createButton('reverse play');
-  button.position(300, 68);
-  button.mousePressed(reverse_speed);
-
-  button = createButton('STOP');
-  button.position(400, 68);
-  button.mousePressed(stop_video);
+  leftVid = document.getElementById("slowly1");
+  rightVid = document.getElementById("slowly2");
 
 }
 
 // plays or pauses the video depending on current state
-// function toggleVid() {
-//   if (playing) {
-//     knock.pause();
-//     button.html('play');
-//   } else {
-//     knock.play();
-//     button.html('pause');
-//   }
-//   playing = !playing;
 
 
-// }
+function draw() {
+  if ((force.right || force.left > 0)) {
+    console.log("right: " + force.right+ " left: " + force.left+ 'greater than 0')
 
+    if (force.right > 0 ) { 
+      slowly2.loop();
+      playing2 = true;
+    } else {
+      slowly2.pause();
+      playing2 = false;
+    }
+
+    if (force.left > 0 ) {
+      slowly1.loop();
+      playing1 = true;
+    } else {
+      slowly1.pause();
+      playing1 = false;
+    } 
+
+    var playingRange = Math.abs(force.right - force.left);
+    if (force.right > force.left) {
+      
+      for (var i = 0; i < playingRange; i ++) {
+        slowly2.speed(i);
+      }
+      
+    } else if (force.right < force.left) {
+      for (var i = 0; i < playingRange; i ++) {
+        slowly1.speed(i);
+      }
+    } else {
+      slowly1.speed(1);
+      slowly2.speed(1);
+    }
+
+  } else {
+    slowly1.pause();
+    slowly2.pause();
+
+    var range = Math.abs(leftVid.currentTime - rightVid.currentTime);
+
+    if (playing1 || playing2 == false) {
+      
+      if (frameCount % 60 == 0) {
+        timer++;
+      }
+      if (1.5 < range < 12.5 ) {
+        if (timer > 3) { // if playing1 & playing2 are set to false for longer than 2 seconds, find the longer one unti it catches up
+          if (leftVid.currentTime < rightVid.currentTime) {
+            slowly1.loop();
+            timer = 0;  
+          } else {
+            slowly2.loop();
+            timer = 0;  
+          }
+        }
+        
+      } else {
+        slowly1.pause();
+        slowly2.pause();
+        timer = 0;
+      }
+    }
+  }
+
+}
+function toggleVid() {
+  if (playing) {
+    knock.pause();
+  } else {
+    knock.play();
+  }
+  playing = !playing;
+}
 function twice_speed() {
     slowly1.speed(2);
     slowly2.speed(2);
@@ -82,8 +138,7 @@ function twice_speed() {
   }
   
   function stop_video() {
-    var vid1 = document.getElementById("slowly1");
-    var vid2 = document.getElementById("slowly2");
+    
     console.log(vid1.currentTime);
     vid2.currentTime;
     
@@ -99,15 +154,15 @@ function twice_speed() {
   }
 
   function dataReceived() {   //this function is called every time data is received
+   
     var rawData = serial.readStringUntil('\r\n'); //read the incoming string until it sees a newline
-    console.log(rawData);   
-    if(rawData.length>1) {                     //check that there is something in the string
-                                              //values received in pairs  index,value
-          var incoming = rawData.split(",");      //split the string into components using the comma as a delimiter
-          for(var i=0;i<incoming.length;i++) {
-            ardVal[i]=parseInt(incoming[i]);        //convert the values to ints and put them into the ardVal array
-          }
-        }
+    // console.log(rawData);                       //uncomment this line to see the incoming string in the console     
+    if(rawData.length>1)                          //check that there is something in the string
+    {                                               
+      force.left = JSON.parse(rawData).s1;            //the name of parameter must match the one created in Arduino
+      force.right = JSON.parse(rawData).s2;            //the name of parameter must match the one created in Arduino
+    }
+    return force;
   }
 
   function ardCon() {
