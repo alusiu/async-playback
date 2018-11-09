bbf35cb7efb54e32a49aa847f2429d6e24c207cb
Olivia Prior <olivialaineprior@gmail.com>
2018-11-08 22:27:44 -0500

    Restructed the way the case statements check to see if the videos are palying

--- sketch.js
+++ sketch.js
@@ -1,18 +1,13 @@
-var playing1 = false;
-var playing2 = false;
-var slowly1;
-var slowly2;
+
+var left;
+var right;
 var force = {};
 var button;
-var playback1;
-var playback2;
-var newPlayback1;
-var nePlayback2;
-var range;
-var leftVid;
-var rightVid;
+var leftPlaying = false;
+var rightPlaying = false;
+
+var playingRange;
 var serial;  //variable to hold the serial port object
-var timer = 0;     // used to time
 var ardVal = [];  //array that will hold all values coming from arduino
 
 var serialPortName = '/dev/cu.usbmodem1411';        //FOR PC it will be COMX on mac it will be something like "/dev/cu.usbmodemXXXX"
@@ -31,16 +26,14 @@ function setup() {
 
   getAudioContext().resume();
   // specify multiple formats for different browsers
-  slowly1 = createVideo(['slowly.mp4']);
-  slowly1.id("slowly1");
-  slowly1.size(360, 360);
+  left = createVideo(['slowly.mp4']);
+  left.id("left");
+  left.size(480, 540);
 
-  slowly2 = createVideo(['slowly.mp4']);
-  slowly2.size(360, 360);
-  slowly2.id("slowly2");
-
-  leftVid = document.getElementById("slowly1");
-  rightVid = document.getElementById("slowly2");
+  right = createVideo(['slowly.mp4']);
+  right.size(480, 540);
+  //left.position();
+  right.id("right");
 
 }
 
@@ -48,108 +41,68 @@ function setup() {
 
 
 function draw() {
-  if ((force.right || force.left > 0)) {
-    console.log("right: " + force.right+ " left: " + force.left+ 'greater than 0')
-    var playingRange = Math.abs(force.right - force.left);
+  
+// find the difference in playing range
+playingRange = Math.abs(force.left-force.right);
 
-    if (force.right > 0 ) { 
-      slowly2.loop();
-      playing2 = true;
+// if left is greater than 0, then 
+if (force.right > 0  && force.left > 0 ) {
+  right.loop();
+  left.loop();
+  leftPlaying = true;
+  rightPlaying = true;
 
-      if (force.right > force.left) {
-      
-        for (var i = 0; i < playingRange; i ++) {
-          slowly2.speed(i);
-        }
-        
-      }
-    } else {
-      slowly2.pause();
-      playing2 = false;
-    }
+} else if (force.right == 0 && force.left > 0){
 
-    if (force.left > 0 ) {
-      slowly1.loop();
+  right.pause();
+  rightPlaying = false;
 
-      if (force.left > force.right) {
-        for (var i = 0; i < playingRange; i ++) {
-          slowly1.speed(i);
-        }
-      playing1 = true;
-    } else {
-      slowly1.pause();
-      playing1 = false;
-    }   
-
+  if (leftPlaying == true ) {
+    left.speed(1.25);
   } else {
-    slowly1.pause();
-    slowly2.pause();
-
-    var range = Math.abs(leftVid.currentTime - rightVid.currentTime);
-
-    if (playing1 || playing2 == false) {
-      
-      if (frameCount % 60 == 0) {
-        timer++;
-      }
-      if (1.5 < range < 12.5 ) {
-        if (timer > 3) { // if playing1 & playing2 are set to false for longer than 2 seconds, find the longer one unti it catches up
-          if (leftVid.currentTime < rightVid.currentTime) {
-            slowly1.loop();
-            timer = 0;  
-          } else {
-            slowly2.loop();
-            timer = 0;  
-          }
-        }
-        
-      } else {
-        slowly1.pause();
-        slowly2.pause();
-        timer = 0;
-      }
-    }
+    left.loop();
+    left.speed(1.25);
+    leftPlaying = true;
+    
   }
-  }
-}
-function toggleVid() {
-  if (playing) {
-    knock.pause();
+} else if (force.right > 0 && force.left == 0){
+
+  left.pause();
+  leftPlaying = false;
+
+  if (rightPlaying == true) {
+    right.speed(1.25);
   } else {
-    knock.play();
+    right.loop();
+    right.speed(1.25);
+    rightPalying = true;
   }
-  playing = !playing;
+  
+} else {
+  right.pause();
+  left.pause();
+
+  rightPlaying = false;
+  leftPlaying = false;
 }
-function twice_speed() {
-    slowly1.speed(2);
-    slowly2.speed(2);
-  }
-  
-  function half_speed() {
-    slowly1.speed(0.5);
-    slowly2.speed(0.5);
-  }
-  
-  function reverse_speed() {
-    slowly1.speed(-1);
-    slowly2.speed(-1);
-  }
-  
-  function stop_video() {
-    
-    console.log(vid1.currentTime);
-    vid2.currentTime;
-    
-    slowly1.pause();
-    slowly2.pause();
-  }
-  
-  function play_speed() {
-    var vid1 = document.getElementById("slowly1");
-    console.log(vid1.duration);
-    slowly1.loop();
-    slowly2.loop();
-  }
+/* if (leftPlaying == false && force.left > 0) {
+    left.loop();
+    leftPlaying = true;
+ } else {
+    left.pause();
+    leftPlaying = false;
+ }
+
+ if (rightPlaying == false && force.right > 0 ) {
+    right.loop();
+    rightPlaying = true;
+ } else {  
+    right.pause(); 
+    rightPlaying = false;
+ }
+*/
+
+}
 
   function dataReceived() {   //this function is called every time data is received
    
